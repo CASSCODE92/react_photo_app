f335d2062049283b10b64b418f341320cde2a66c
CASSCODE92 <cassie2012abioye@gmail.com>
2020-07-26 14:31:12 -0400

    Shark Puppet Profile Page Link

--- src/profiles.js
+++ src/profiles.js
@@ -9,7 +9,7 @@ ReactDom.render(
     <ul className = "profiles">
                 <li className = "Profile1"><a href = "photo-app1.js"><img src= "doge1.jpg">Profile 1</a></li>
                 <li className = "Profile2"><a href = "photo-app2.js"><img src= "mayo1.jpg"></img>Profile 2</a></li>
-                <li className = "Profile3"><a href = "photo-app3.js"><img src= ""></img>Profile 3</a></li>
+                <li className = "Profile3"><a href = "photo-app3.js"><img src= "shark1.jpg"></img>Profile 3</a></li>
                 
     </ul>,
     
