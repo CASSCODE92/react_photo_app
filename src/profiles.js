52a58c09ee139159d9c1862eccf969d1b200e157
CASSCODE92 <cassie2012abioye@gmail.com>
2020-07-26 02:54:27 -0400

    Profile3 Link

--- src/profiles.js
+++ src/profiles.js
@@ -9,7 +9,7 @@ ReactDom.render(
     <ul className = "profiles">
                 <li className = "Profile1"><a href = "home.js">HOME</a></li>
                 <li className = "Profile2"><a href = "profiles.js">PROFILES</a></li>
-                <li className = "navList"><a href = "bookmarks.js">BOOKMARKS</a></li>
+                <li className = "Profile3"><a href = "bookmarks.js">BOOKMARKS</a></li>
                 <li className = "navList"><a href = "log_in.js">LOGIN</a></li>
     </ul>,
     
