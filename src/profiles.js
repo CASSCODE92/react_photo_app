10d85d010ee5f56461cc66e600296280cc20c108
CASSCODE92 <cassie2012abioye@gmail.com>
2020-07-26 14:34:54 -0400

    Profile Page Lil' Mayo Name

--- src/profiles.js
+++ src/profiles.js
@@ -8,7 +8,7 @@ import './photo-app3.js';
 ReactDom.render(
     <ul className = "profiles">
                 <li className = "Profile1"><a href = "photo-app1.js"><img src= "doge1.jpg"><h1>Doge</h1></a></li>
-                <li className = "Profile2"><a href = "photo-app2.js"><img src= "mayo1.jpg"></img>Profile 2</a></li>
+                <li className = "Profile2"><a href = "photo-app2.js"><img src= "mayo1.jpg"></img><h1>Lil' Mayo</h1></a></li>
                 <li className = "Profile3"><a href = "photo-app3.js"><img src= "shark1.jpg"></img>Profile 3</a></li>
                 
     </ul>,
