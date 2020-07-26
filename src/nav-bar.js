ff530ae7431276285bf94998c988b5b2141a09f5
CASSCODE92 <cassie2012abioye@gmail.com>
2020-07-26 02:02:18 -0400

    className navList Profiles

--- src/nav-bar.js
+++ src/nav-bar.js
@@ -6,7 +6,7 @@ import './index.css';
 ReactDom.render(
 <ul className = "navBar">
             <li className = "navList"><a href = "home.js">HOME</a></li>
-            <li><a href = "profiles.js">PROFILES</a></li>
+            <li className = "navList"><a href = "profiles.js">PROFILES</a></li>
             <li><a href = "bookmarks.js">BOOKMARKS</a></li>
             <li><a href = "log_in.js">LOGIN</a></li>
 </ul>,
