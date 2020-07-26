c350528060779414ca0cb01064f22f884795036a
CASSCODE92 <cassie2012abioye@gmail.com>
2020-07-26 00:24:09 -0400

    return components profile 2

--- src/photo-app2.js
+++ src/photo-app2.js
@@ -2,37 +2,46 @@ import React from "react";
 import ReactDom from "react-dom";
 import './index.css';
 
-<li><a href = "nav-bar.js">NAVBAR</a></li>
+class PhotoApp1 extends React.Component{
 
-const img = ""
-
-
-ReactDom.render(
+    render(){
+        const img = "";
+    
+    return(
+        <li><a href = "nav-bar.js">NAVBAR</a></li>
+    
+        <div>
+        <img src = {img}/>
+        </div>
+    
+        <div>
+            <h1 className = "name1"></h1>
+        </div>
+    
     <div>
-      <img src = {img}/>
+        <img src="">
     </div>
-
-<div>
-<h1 className = "name2"></h1>
-</div>
-
-
-<div>
-    <img src="">
-</div>
-
-<div>
-    <img src="">
-</div>
-
-<div>
-    <img src="">
-</div>
-
-<div>
-    <img src="">
-</div>
-
-<div>
-    <img src="">
-</div>
+    
+    <div>
+        <img src="">
+    </div>
+    
+    <div>
+        <img src="">
+    </div>
+    
+    <div>
+        <img src="">
+    </div>
+    
+    <div>
+        <img src="">
+    </div>
+    
+    );
+    }
+    }
+    
+    ReactDOM.render(<PhotoApp1 />, document.getElementById("root"));
+    
+    
