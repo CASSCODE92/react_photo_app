07079526d66e88cde0a36b53433d1d8c61af0752
CASSCODE92 <cassie2012abioye@gmail.com>
2020-07-26 02:53:36 -0400

    Profile2 Link

--- src/profiles.js
+++ src/profiles.js
@@ -8,7 +8,7 @@ import './photo-app3.js';
 ReactDom.render(
     <ul className = "profiles">
                 <li className = "Profile1"><a href = "home.js">HOME</a></li>
-                <li className = "navList"><a href = "profiles.js">PROFILES</a></li>
+                <li className = "Profile2"><a href = "profiles.js">PROFILES</a></li>
                 <li className = "navList"><a href = "bookmarks.js">BOOKMARKS</a></li>
                 <li className = "navList"><a href = "log_in.js">LOGIN</a></li>
     </ul>,
