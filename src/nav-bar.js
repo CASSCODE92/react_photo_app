0c924e547b08bd5a344981054c0d1dfc8f6ebf8b
CASSCODE92 <cassie2012abioye@gmail.com>
2020-07-26 02:03:34 -0400

    className navList Bookmarks

--- src/nav-bar.js
+++ src/nav-bar.js
@@ -7,7 +7,7 @@ ReactDom.render(
 <ul className = "navBar">
             <li className = "navList"><a href = "home.js">HOME</a></li>
             <li className = "navList"><a href = "profiles.js">PROFILES</a></li>
-            <li><a href = "bookmarks.js">BOOKMARKS</a></li>
+            <li className = "navList"><a href = "bookmarks.js">BOOKMARKS</a></li>
             <li><a href = "log_in.js">LOGIN</a></li>
 </ul>,
 
